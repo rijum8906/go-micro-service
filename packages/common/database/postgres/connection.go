f4a158d8a8016bbefd8aea6cc53dfae925a3582a
Riju Mondal <rijum8906@gmail.com>
2026-01-24 05:43:48 +0530

    feat(common-db-pg): edit connect func to return connection pool

--- packages/common/database/postgres/connection.go
+++ packages/common/database/postgres/connection.go
@@ -1,28 +1,58 @@
-// Package postgres
 package postgres
 
 import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type PGXConfig struct {
+type Config struct {
 	User     string
 	Password string
 	Host     string
 	Port     int
 	Database string
-	SSLMode  bool
+	SSLMode  string // disable | require | verify-ca | verify-full
 }
 
-func ConnectDB(ctx context.Context, cfg PGXConfig) *pgx.Conn {
-	connStr := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%t", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.Database, cfg.SSLMode)
-	conn, err := pgx.Connect(ctx, connStr)
+// DSN builds a postgres connection string
+func (c Config) DSN() string {
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s:%d/%s?sslmode=%s",
+		c.User,
+		c.Password,
+		c.Host,
+		c.Port,
+		c.Database,
+		c.SSLMode,
+	)
+}
+
+// Connect creates and returns a pgx connection pool
+func Connect(ctx context.Context, cfg Config) *pgxpool.Pool {
+	poolConfig, err := pgxpool.ParseConfig(cfg.DSN())
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("failed to parse db config: %v", err)
 	}
-	return conn
+
+	// Sensible defaults (you can tune later)
+	poolConfig.MaxConns = 10
+	poolConfig.MinConns = 2
+	poolConfig.MaxConnIdleTime = 5 * time.Minute
+	poolConfig.MaxConnLifetime = 30 * time.Minute
+
+	pool, err := pgxpool.NewWithConfig(ctx, poolConfig)
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	// Verify connection early (fail fast)
+	if err := pool.Ping(ctx); err != nil {
+		log.Fatalf("failed to ping database: %v", err)
+	}
+
+	return pool
 }
