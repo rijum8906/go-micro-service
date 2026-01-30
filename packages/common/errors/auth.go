6895356045ee40988615726f117392820bc72b39
Riju Mondal <Riju Mondal>
2026-01-30 11:02:57 +0530

    feat(common): added new errors in errors package

--- packages/common/errors/auth.go
+++ packages/common/errors/auth.go
@@ -7,9 +7,20 @@ var (
 	ErrAccountNotFound    = errors.New("account not found")
 	ErrProfileNotFound    = errors.New("profile not found")
 	ErrOAuthNotFound      = errors.New("oauth not found")
+	ErrAccountExists      = errors.New("account already exists")
+
+	// Refresh token
+	ErrInvalidRefreshToken = errors.New("invalid refresh token")
+	ErrExpiredRefreshToken = errors.New("refresh token expired")
 
 	// Token
 	ErrInvalidToken       = errors.New("invalid token")
 	ErrExpiredToken       = errors.New("token expired")
 	ErrInvalidTokenClaims = errors.New("invalid token claims")
+	ErrTokenRevoked       = errors.New("token revoked")
+
+	// Session
+	ErrInvalidSession  = errors.New("invalid session")
+	ErrSessionExpired  = errors.New("session expired")
+	ErrSessionNotFound = errors.New("session not found")
 )
