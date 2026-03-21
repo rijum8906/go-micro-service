6735f80b0ebecff70fbe4a3c2f1d0fd89b55e74a
Riju Mondal <rijum8906@gmail.com>
2026-03-22 00:24:00 +0530

    feat(core): add some common errors and codes in apperror

--- packages/core/apperror/common.go
+++ packages/core/apperror/common.go
@@ -1,1 +1,45 @@
 package apperror
+
+// Codes
+const (
+	CodeInternal        = "INTERNAL_ERROR"
+	CodeNotFound        = "NOT_FOUND"
+	CodeValidation      = "VALIDATION_FAILED"
+	CodeUnAuthenticated = "UNAUTHENTICATED"
+	CodeForbidden       = "FORBIDDEN"
+	CodeThirdParty      = "EXTERNAL_SERVICE_ERROR"
+)
+
+// Common Errors
+var (
+	ErrInternal = &AppError{
+		Type:    TypeInternal,
+		Code:    CodeInternal,
+		Message: "Internal Server Error",
+	}
+	ErrorForbidden = &AppError{
+		Type:    TypeForbidden,
+		Code:    CodeForbidden,
+		Message: "Forbidden",
+	}
+	ErrNotFound = &AppError{
+		Type:    TypeNotFound,
+		Code:    CodeNotFound,
+		Message: "Not Found",
+	}
+	ErrUnAuthenticated = &AppError{
+		Type:    TypeUnAuthenticated,
+		Code:    CodeUnAuthenticated,
+		Message: "Unauthenticated",
+	}
+	ErrThirdParty = &AppError{
+		Type:    TypeThirdParty,
+		Code:    CodeThirdParty,
+		Message: "External Service Error",
+	}
+	ErrValidation = &AppError{
+		Type:    TypeValidation,
+		Code:    CodeValidation,
+		Message: "Validation Failed",
+	}
+)
