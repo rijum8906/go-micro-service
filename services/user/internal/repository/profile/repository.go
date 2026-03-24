55f6b2e79dc55b06688bea2b1dcc7118c9c881a3
Riju Mondal <rijum8906@gmail.com>
2026-03-24 13:50:11 +0530

    feat(service-user): add repository for profile, user and session
    1. Added functions to create, fetch , update and delete user, profile
    and session tables

---
+++ services/user/internal/repository/profile/repository.go
@@ -0,0 +1,56 @@
+package profile
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/rijum8906/relay/packages/core/apperror"
+	"github.com/rijum8906/relay/services/user/internal/db"
+)
+
+func (r *profileRepository) GetProfile(ctx context.Context, id uuid.UUID) (*db.Profile, *apperror.AppError) {
+	profile, err := r.q.GetProfile(ctx, id)
+	if err != nil {
+		return nil, apperror.ErrInternal.WithMessage("Failed to get profile").WithDetail("error", err.Error())
+	}
+	return &profile, nil
+}
+
+func (r *profileRepository) GetProfileByUserID(ctx context.Context, id uuid.UUID) (*db.Profile, *apperror.AppError) {
+	profile, err := r.q.GetProfileByUserID(ctx, id)
+	if err != nil {
+		return nil, apperror.ErrInternal.WithMessage("Failed to get profile").WithDetail("error", err.Error())
+	}
+	return &profile, nil
+}
+
+func (r *profileRepository) UpdateProfileNames(ctx context.Context, id uuid.UUID, firstName, lastName string) (*db.Profile, *apperror.AppError) {
+	profile, err := r.q.UpdateProfile(ctx, db.UpdateProfileParams{
+		ID:        id,
+		FirstName: firstName,
+		LastName:  lastName,
+	})
+	if err != nil {
+		return nil, apperror.ErrInternal.WithMessage("Failed to update profile").WithDetail("error", err.Error())
+	}
+	return &profile, nil
+}
+
+func (r *profileRepository) UpdateProfileAvatar(ctx context.Context, id uuid.UUID, avatar string) (*db.Profile, *apperror.AppError) {
+	profile, err := r.q.UpdateProfile(ctx, db.UpdateProfileParams{
+		ID:        id,
+		AvatarUrl: avatar,
+	})
+	if err != nil {
+		return nil, apperror.ErrInternal.WithMessage("Failed to update profile").WithDetail("error", err.Error())
+	}
+	return &profile, nil
+}
+
+func (r *profileRepository) DeleteProfile(ctx context.Context, id uuid.UUID) *apperror.AppError {
+	err := r.q.DeleteProfile(ctx, id)
+	if err != nil {
+		return apperror.ErrInternal.WithMessage("Failed to delete profile").WithDetail("error", err.Error())
+	}
+	return nil
+}
