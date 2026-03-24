cc9a522dd760cac0f9b943baf6916862cf0487f6
Riju Mondal <rijum8906@gmail.com>
2026-03-24 13:50:11 +0530

    feat(service-user): add repository for profile, user and session
    1. Added functions to create, fetch , update and delete user, profile
    and session tables

---
+++ services/user/internal/repository/user/interface.go
@@ -0,0 +1,30 @@
+// Package user
+package user
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/rijum8906/relay/packages/core/apperror"
+	"github.com/rijum8906/relay/services/user/internal/db"
+	dto "github.com/rijum8906/relay/services/user/internal/dto/auth"
+)
+
+type UserRepository interface {
+	CreateUser(ctx context.Context, data *dto.Register) (*db.User, *apperror.AppError)
+	UpdateUserPassword(ctx context.Context, id uuid.UUID, newPassHash string) *apperror.AppError
+	UpdateUserEmail(ctx context.Context, id uuid.UUID, email string) *apperror.AppError
+	GetUser(ctx context.Context, id uuid.UUID) (*db.User, *apperror.AppError)
+	GetUserByEmail(ctx context.Context, email string) (*db.User, *apperror.AppError)
+	DeleteUser(ctx context.Context, id uuid.UUID) *apperror.AppError
+}
+
+type userRepository struct {
+	q db.Querier
+}
+
+func NewAuthRepository(q db.Querier) UserRepository {
+	return &userRepository{
+		q: q,
+	}
+}
