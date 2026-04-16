a2976fce9280bf726bb11d40f3ceaa145d4ef725
Legaz-le <188694281+Legaz-le@users.noreply.github.com>
2026-04-16 20:08:25 +0900

    refactor(notification-service): move NATS subscriptions into subscriber service

--- services/notification-service/internal/services/subscriber/service.go
+++ services/notification-service/internal/services/subscriber/service.go
@@ -1,5 +1,4 @@
-// Package broker
-package broker
+package subscriber
 
 import (
 	"encoding/json"
@@ -14,14 +13,23 @@ import (
 	"github.com/rijum8906/relay/services/notification-service/internal/utils"
 )
 
-type SubscribeHandler struct {
-	EmailService    email.Service
-	NatsClient      *nats.Client
+type service struct {
+	emailService    email.Service
+	natsClient      *nats.Client
 	templateManager template.TemplateManager
 	mailerCfg       *mailer.Config
 }
 
-func New(emailService email.Service, client *nats.Client, mailerCfg *mailer.Config) (*SubscribeHandler, *apperror.AppError) {
+func New(emailService email.Service, natsClient *nats.Client, mailerCfg *mailer.Config) (Service, *apperror.AppError) {
+	if emailService == nil {
+		return nil, apperror.ErrInternal.WithMessage("email service is not initialized")
+	}
+	if natsClient == nil {
+		return nil, apperror.ErrInternal.WithMessage("nats client  is not initialized")
+	}
+	if mailerCfg == nil {
+		return nil, apperror.ErrInternal.WithMessage("mailer  is not initialized")
+	}
 	tm, err := template.NewTemplateManagerWithCompanyInfo("packages/templates", &dto.CompanyInfo{
 		Name:       "Relay",
 		Emails:     []string{"UfNwO@example.com"},
@@ -38,21 +46,20 @@ func New(emailService email.Service, client *nats.Client, mailerCfg *mailer.Conf
 		fmt.Println("Error creating template manager:", err)
 		return nil, apperror.ErrInternal.WithMessage("failed to create template manager").WithDetail("error", err.Error())
 	}
-
-	return &SubscribeHandler{
-		EmailService:    emailService,
-		NatsClient:      client,
-		mailerCfg:       mailerCfg,
+	return &service{
+		emailService:    emailService,
+		natsClient:      natsClient,
 		templateManager: tm,
+		mailerCfg:       mailerCfg,
 	}, nil
 }
 
-func (h *SubscribeHandler) Subscribe() *apperror.AppError {
+func (h *service) Subscribe() *apperror.AppError {
 	if h == nil {
 		return apperror.ErrInternal.WithMessage("subscribe handler is not initialized")
 	}
 
-	if h.NatsClient == nil {
+	if h.natsClient == nil {
 		return apperror.ErrInternal.WithMessage("nats client is not initialized")
 	}
 
@@ -69,7 +76,7 @@ func (h *SubscribeHandler) Subscribe() *apperror.AppError {
 		return appErr
 	}
 
-	h.NatsClient.Subscribe(dto.JobEmailVerification, func(b []byte) {
+	_, appErr = h.natsClient.Subscribe(dto.JobEmailVerification, func(b []byte) {
 		var data dto.EmailVerificationDTO
 		err := json.Unmarshal(b, &data)
 		if err != nil {
@@ -108,6 +115,52 @@ func (h *SubscribeHandler) Subscribe() *apperror.AppError {
 			return
 		}
 	})
+	if appErr != nil {
+		return appErr
+	}
+
+	_, appErr = h.natsClient.Subscribe(dto.JobEmailPasswordReset, func(b []byte) {
+		var data dto.PasswordResetDTO
+		err := json.Unmarshal(b, &data)
+		if err != nil {
+			// TODO: save to some logs
+			fmt.Println("Error unmarshalling job:", err)
+			return
+		}
+
+		emailTemplate, appErr := h.templateManager.RenderToString(template.TemplateTypeEmailPasswordReset, data)
+		if appErr != nil {
+			// TODO: save to some logs
+			fmt.Println("Error rendering email template:", appErr.Details)
+			return
+		}
+
+		envelop, appErr := utils.ParseMailEnvelop(h.mailerCfg, data.ClientEmail)
+		if appErr != nil {
+			// TODO: save to some logs
+			fmt.Println("Error parsing mail envelop:", appErr)
+			return
+		}
+
+		appErr = mailer.Send(client, mailer.Message{
+			Envelope: envelop,
+			Content: mailer.Content{
+				HTML:        emailTemplate,
+				Subject:     "Password Reset",
+				Priority:    mailer.EmailPriorityHigh,
+				Attachments: []mailer.Attachment{},
+				Headers:     map[string]string{},
+			},
+		})
+		if appErr != nil {
+			// TODO: save to some logs
+			fmt.Println("Error sending email:", appErr.Details)
+			return
+		}
+	})
+	if appErr != nil {
+		return appErr
+	}
 
 	return nil
 }
